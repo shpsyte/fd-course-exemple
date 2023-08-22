f1e43f405c15e80750a2694fe20c199ebdb68bc0
luiz.j <jose.iscosistemas@gmail.com>
2023-08-22 21:53:51 +0000

    Adicionando um test para testat o jest

---
+++ tests/calculadora.test.js
@@ -0,0 +1,15 @@
+// import { test, expect } from "jest";
+
+test(" espero que 1 seja 1", () => {
+  const esperado = 10;
+  const test = 10;
+  expect(test).toBe(esperado);
+});
+
+// test("nome do teste", () => {
+//   console.log("This is a simple test");
+// });
+
+// test("testando outra condicao", () => {
+//   console.log(`Esse eh outro teste`);
+// });
