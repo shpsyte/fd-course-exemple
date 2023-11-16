f378a9ce70a2dcacd518db00b2c326c463f731ce
luiz.j <jose.iscosistemas@gmail.com>
2023-11-16 22:31:12 +0000

    add jsconfig

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,9 @@
-import database from '../../../../infra/database.js';
+import database from 'infra/database';
 
 
 export default async function status(req, res) {
   const result = await database.query('SELECT 1 + 1 as sum');
-  console.log(result.rows[0].sum);
+  // console.log(result.rows[0].sum);
   res.status(200).json({ status: 'ok ne' });
 }
 
