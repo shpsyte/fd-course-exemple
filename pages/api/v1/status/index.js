eecfa95dc37b08a0c6b33c9eb6c7fe681ad78b24
luiz.j <jose.iscosistemas@gmail.com>
2023-12-06 15:36:01 +0000

    adding version, max_connextion  to `status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,27 @@
-import database from 'infra/database';
-
+import database from "infra/database";
 
 export default async function status(req, res) {
-  const result = await database.query('SELECT 1 + 1 as sum');
-  // console.log(result.rows[0].sum);
-  res.status(200).json({ status: 'ok ne' });
-}
+  const updatedAt = new Date().toISOString();
+  let query = await database.query("SHOW server_version;");
 
-// curl -s -o /dev/null -I -w "%{http_code}" http://localhost:3000/api/status | grep 200
+  const databaseVersionValue = query.rows[0].server_version;
+  const databaseMaxConnextion = +(await database.query("SHOW max_connections;"))
+    .rows[0].max_connections;
+  const databaseCurrentConnextion = +(
+    await database.query(
+      "SELECT count(*) FROM pg_stat_activity  WHERE state = 'active';"
+    )
+  ).rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnextion,
+        current_connections: databaseCurrentConnextion,
+      },
+    },
+  });
+}
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,12 +1,21 @@
+test("Get to /api/v2/status should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
 
+  const expectedStatusCode = 200;
+  const actualStatusCode = response.status;
+  expect(actualStatusCode).toBe(expectedStatusCode);
 
-test('Get to /api/v2/status should return 200', async () => {
-  const response = await fetch('http://localhost:3000/api/v1/status')
+  const responseJson = await response.json();
 
-  const expectedStatusCode = 200
-  const actualStatusCode = response.status
+  expect(responseJson.updated_at).toBeDefined();
+  const dateParsedAt = new Date(responseJson.updated_at).toISOString();
+  expect(responseJson.updated_at).toEqual(dateParsedAt);
 
-  expect(actualStatusCode).toBe(expectedStatusCode)
-  // expect(response.statusCode).toBe(200)
-  // expect(response.body.status).toBe('ok')
-})
+  // verison
+  expect(responseJson.dependencies.database.version).toEqual("16.0");
+
+  expect(responseJson.dependencies.database.max_connections).toBeDefined();
+  expect(responseJson.dependencies.database.max_connections).toBeGreaterThan(0);
+
+  expect(responseJson.dependencies.database.current_connections).toBeDefined();
+});
