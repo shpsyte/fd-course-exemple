117d6f8e7374ec567dbb6dcf48ee19551620d702
luiz.j <jose.iscosistemas@gmail.com>
2023-12-08 15:14:15 +0000

    adding teste

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -7,6 +7,7 @@ export default async function status(req, res) {
   const databaseVersionValue = query.rows[0].server_version;
   const databaseMaxConnextion = +(await database.query("SHOW max_connections;"))
     .rows[0].max_connections;
+
   const databaseCurrentConnextion = +(
     await database.query(
       "SELECT count(*) FROM pg_stat_activity  WHERE state = 'active';"
@@ -21,6 +22,7 @@ export default async function status(req, res) {
         version: databaseVersionValue,
         max_connections: databaseMaxConnextion,
         current_connections: databaseCurrentConnextion,
+        email: "asdfa",
       },
     },
   });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,15 +7,39 @@ test("Get to /api/v2/status should return 200", async () => {
 
   const responseJson = await response.json();
 
-  expect(responseJson.updated_at).toBeDefined();
+  console.log(responseJson);
+
+  // {
+  //   updated_at: '2023-12-08T14:45:21.779Z',
+  //   dependencies: {
+  //     database: { version: '16.0', max_connections: 100, current_connections: 1 }
+  //   }
+  // }
   const dateParsedAt = new Date(responseJson.updated_at).toISOString();
-  expect(responseJson.updated_at).toEqual(dateParsedAt);
 
-  // verison
-  expect(responseJson.dependencies.database.version).toEqual("16.0");
+  // validate the shape of the response
+  const expectedResponseJson = {
+    updated_at: expect.any(String),
+    dependencies: expect.objectContaining({
+      database: expect.objectContaining({
+        version: expect.any(String),
+        max_connections: expect.any(Number),
+        current_connections: expect.any(Number),
+      }),
+    }),
+  };
 
-  expect(responseJson.dependencies.database.max_connections).toBeDefined();
-  expect(responseJson.dependencies.database.max_connections).toBeGreaterThan(0);
+  expect(responseJson).toMatchObject(expectedResponseJson);
 
-  expect(responseJson.dependencies.database.current_connections).toBeDefined();
+  // expect(responseJson.updated_at).toBeDefined();
+  // const dateParsedAt = new Date(responseJson.updated_at).toISOString();
+  // expect(responseJson.updated_at).toEqual(dateParsedAt);
+
+  // // verison
+  // expect(responseJson.dependencies.database.version).toEqual("16.0");
+
+  // expect(responseJson.dependencies.database.max_connections).toBeDefined();
+  // expect(responseJson.dependencies.database.max_connections).toBeGreaterThan(0);
+
+  // expect(responseJson.dependencies.database.current_connections).toBeDefined();
 });
