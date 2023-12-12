83ba1b68cab117e69d55f03c26d8c6a5380bd846
luiz.j <jose.iscosistemas@gmail.com>
2023-12-12 17:17:26 +0000

    validating db

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,16 +3,16 @@ import database from "infra/database";
 export default async function status(req, res) {
   const updatedAt = new Date().toISOString();
   let query = await database.query("SHOW server_version;");
-
   const databaseVersionValue = query.rows[0].server_version;
-  const databaseMaxConnextion = +(await database.query("SHOW max_connections;"))
-    .rows[0].max_connections;
+  query = await database.query("SHOW max_connections;");
+  const databaseMaxConValue = +query.rows[0].max_connections;
 
-  const databaseCurrentConnextion = +(
-    await database.query(
-      "SELECT count(*) FROM pg_stat_activity  WHERE state = 'active';"
-    )
-  ).rows[0].count;
+  query = await database.query(`SELECT count(*)::int 
+                                  FROM pg_stat_activity  
+                                 WHERE 1=1
+                                   AND datname = 'local_db';`);
+
+  const databaseCurrentConnextion = query.rows[0].count;
 
   res.status(200).json({
     updated_at: updatedAt,
@@ -20,9 +20,9 @@ export default async function status(req, res) {
     dependencies: {
       database: {
         version: databaseVersionValue,
-        max_connections: databaseMaxConnextion,
-        current_connections: databaseCurrentConnextion,
-        email: "asdfa",
+        max_connections: databaseMaxConValue,
+        open_connections: databaseCurrentConnextion,
+        // email: "asdfa",
       },
     },
   });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -9,37 +9,41 @@ test("Get to /api/v2/status should return 200", async () => {
 
   console.log(responseJson);
 
-  // {
-  //   updated_at: '2023-12-08T14:45:21.779Z',
-  //   dependencies: {
-  //     database: { version: '16.0', max_connections: 100, current_connections: 1 }
-  //   }
-  // }
-  const dateParsedAt = new Date(responseJson.updated_at).toISOString();
+  // const dateParsedAt = new Date(responseJson.updated_at).toISOString();
 
   // validate the shape of the response
-  const expectedResponseJson = {
-    updated_at: expect.any(String),
-    dependencies: expect.objectContaining({
-      database: expect.objectContaining({
-        version: expect.any(String),
-        max_connections: expect.any(Number),
-        current_connections: expect.any(Number),
-      }),
-    }),
-  };
+  // const expectedResponseJson = {
+  //   updated_at: expect.any(String),
+  //   dependencies: expect.objectContaining({
+  //     database: expect.objectContaining({
+  //       version: expect.any(String),
+  //       max_connections: expect.any(Number),
+  //       current_connections: expect.any(Number),
+  //     }),
+  //   }),
+  // };
 
-  expect(responseJson).toMatchObject(expectedResponseJson);
+  expect(responseJson).toMatchObject(
+    expect.objectContaining({
+      updated_at: expect.any(String),
+      dependencies: {
+        database: {
+          version: "16.0",
+          max_connections: expect.any(Number),
+          open_connections: 1,
+        },
+      },
+    })
+  );
+
+  // expect(responseJson).toMatchObject(expectedResponseJson);
 
   // expect(responseJson.updated_at).toBeDefined();
   // const dateParsedAt = new Date(responseJson.updated_at).toISOString();
   // expect(responseJson.updated_at).toEqual(dateParsedAt);
-
   // // verison
   // expect(responseJson.dependencies.database.version).toEqual("16.0");
-
   // expect(responseJson.dependencies.database.max_connections).toBeDefined();
   // expect(responseJson.dependencies.database.max_connections).toBeGreaterThan(0);
-
-  // expect(responseJson.dependencies.database.current_connections).toBeDefined();
+  // expect(responseJson.dependencies.database.open_connections).toBe(1);
 });
