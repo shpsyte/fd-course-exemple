8e912b0f010947cbc86e4129e623cc1d2cadcc63
luiz.j <jose.iscosistemas@gmail.com>
2024-03-20 16:52:00 +0000

    wit: test config

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,13 +1,21 @@
+import database from "infra/database";
+
 test("Get to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   // console.log(response);
+  console.log(process.env.NODE_ENV);
+  console.log(process.env.DATABASE_URL);
+
+  // const q = "SELECT * FROM pgmigrations";
+  // const result = await database.query(q);
+  // console.log(result);
 
   const expectedStatusCode = 200;
   const actualStatusCode = response.status;
   expect(actualStatusCode).toBe(expectedStatusCode);
 
   const responseBody = await response.json();
-  console.log(responseBody);
 
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,15 @@
+test("Get to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  // console.log(response);
+
+  const expectedStatusCode = 200;
+  const actualStatusCode = response.status;
+  expect(actualStatusCode).toBe(expectedStatusCode);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+
+  expect(Array.isArray(responseBody)).toBe(true);
+});
