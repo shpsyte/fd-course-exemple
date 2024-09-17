aa1231b1e9d4717a47686bd8569c697a505fb06f
luiz.j <jose.iscosistemas@gmail.com>
2024-09-17 14:09:45 +0000

    chore: renaming test to adhere convention

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,27 +1,20 @@
 import orchestrator from "tests/orchestrator.js";
-import database from "infra/database";
+
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await cleanDatabase();
+  await orchestrator.cleanDatabase();
 });
 
-async function cleanDatabase() {
-  const q = "drop schema public cascade; create schema public;";
-  await database.query(q);
-}
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-test("Get to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  // const q = "SELECT * FROM pgmigrations";
-  // const result = await database.query(q);
-  // console.log(result);
+      const responseBody = await response.json();
 
-  const expectedStatusCode = 200;
-  const actualStatusCode = response.status;
-  expect(actualStatusCode).toBe(expectedStatusCode);
-
-  const responseBody = await response.json();
-
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,4 +1,5 @@
 import retry from "async-retry";
+import database from "infra/database.js";
 async function waitForAllServices() {
   await waitForWebServer();
 
@@ -18,7 +19,11 @@ async function waitForAllServices() {
     }
   }
 }
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
 const orchestrator = {
   waitForAllServices: waitForAllServices,
+  cleanDatabase,
 };
 export default orchestrator;
