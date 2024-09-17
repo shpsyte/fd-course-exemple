db5bae021a7f1bbf0603e963cad0a8a3fc0e8f91
luiz.j <jose.iscosistemas@gmail.com>
2024-09-17 14:41:04 +0000

    fix: fix getting direc from migration

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,4 +1,4 @@
-import { join } from "node:path";
+import { resolve } from "node:path";
 import migrationRunner from "node-pg-migrate";
 import database from "infra/database";
 
@@ -17,7 +17,7 @@ export default async function migrations(req, res) {
     const defaultMigration = {
       dbClient: dbClient,
       dryRun: false,
-      dir: join("infra", "migrations"),
+      dir: resolve("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
